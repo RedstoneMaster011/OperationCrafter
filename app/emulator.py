f65382b93b75facf28b4b6c1e972ef7886c92244
RedstoneMaster011 <redstonemaster109lukas@gmail.com>
2026-02-11 21:24:55 -0500

    v1.1 oops, broke linux emulating (again). reverted. fully

--- app/emulator.py
+++ app/emulator.py
@@ -35,12 +35,21 @@ class OSLauncher:
 
         terminal_callback("Starting Emulation.")
 
-        cmd = [
-            self.qemu_path,
-            "-drive", f"format=raw,file={img_path}",
-            "-audiodev", "dsound,id=snd0",
-            "-machine", "pcspk-audiodev=snd0"
-        ]
+
+        if sys.platform == 'win32':
+            cmd = [
+                self.qemu_path,
+                "-drive", f"format=raw,file={img_path}",
+                "-audiodev", "dsound,id=snd0",
+                "-machine", "pcspk-audiodev=snd0"
+            ]
+        else:
+            cmd = [
+                self.qemu_path,
+                "-drive", f"format=raw,file={img_path}",
+                "-audiodev", "pa,id=snd0",
+                "-machine", "pcspk-audiodev=snd0"
+            ]
 
         executable = cmd[0] if os.path.exists(self.qemu_path) else "qemu-system-x86_64"
 
