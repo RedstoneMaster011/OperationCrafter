477bd6e02619036eea9ad1ee70c56d62e2c110f1
RedstoneMaster011 <redstonemaster109lukas@gmail.com>
2026-02-14 16:46:10 -0500

    v1.3 BUGFIXES WITH VARS! all vars are now local. (testing now)

--- app/block.py
+++ app/block.py
@@ -365,9 +365,9 @@ class BlockCanvas(QGraphicsScene):
                 var = "0"
             if "0" not in var:
                 if "db" in line or "dw" in line:
-                    split_code[inde-1] = line.replace(f"'%var[{var}]'", var).replace("db", "equ").replace("dw", "equ").replace(", 0", "")
+                    split_code[inde-1] = line.replace(f"'%var[{var}]'", "."+var).replace("db", "equ").replace("dw", "equ").replace(", 0", "")
                 else:
-                    split_code[inde - 1] = line.replace(f"'%var[{var}]'", var)
+                    split_code[inde - 1] = line.replace(f"'%var[{var}]'", "."+var)
 
             if "%var[" in line and "'%var[" not in line:
                 if "]" in line and "]'" not in line:
@@ -380,9 +380,9 @@ class BlockCanvas(QGraphicsScene):
                 var = "0"
             if "0" not in var:
                 if "db" in line or "dw" in line:
-                    split_code[inde-1] = line.replace(f"%var[{var}]", var).replace("db", "equ").replace("dw", "equ").replace(", 0", "")
+                    split_code[inde-1] = line.replace(f"%var[{var}]", "."+var).replace("db", "equ").replace("dw", "equ").replace(", 0", "")
                 else:
-                    split_code[inde - 1] = line.replace(f"%var[{var}]", var)
+                    split_code[inde - 1] = line.replace(f"%var[{var}]", "."+var)
 
         full_code = "\n".join(split_code)
 
